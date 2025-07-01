396f07c38a0e549375bb57c32de1455dd1dc8c63
davidvegacorus <david.vega@corusconsulting.com>
2025-07-01 08:04:16 -0500

    mejora salida para render out/

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  output: 'export',
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
